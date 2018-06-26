4b94969d1a46102fb6e4ca6f3ecf3904fc6615b1
Praharshit Sharma <bioinformer@users.noreply.github.com>
2018-06-26 23:14:14 +0530

    Create cnfmtx.R

---
+++ cnfmtx.R
@@ -0,0 +1,24 @@
+install.packages("seqinr")
+library("seqinr")
+disc1 <- read.fasta(file = "sequence.fasta")
+getAnnot(disc1)
+seqlen <- summary(disc1[[1]])$length
+seqlen
+nt1 <- disc1[[1]]
+count(nt1,1)
+a <- 115333
+c <- 85887
+g <- 91918
+t <- 128320
+cnt <- c(a,c,t,g)
+cnt
+nrs <- gl(4, 3, labels = cnt)
+nrs
+baseparam1 <- combn(cnt[1:4],2)
+baseparam1
+baseparam2 <- combn(cnt[4:1],2)
+baseparam2
+baseparam <- cbind(baseparam1,baseparam2)
+baseparam
+allratio <- as.numeric(levels(nrs))[nrs]/colSums(baseparam)
+allratio
